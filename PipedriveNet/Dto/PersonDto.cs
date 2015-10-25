a0f38483eeafd2262c4e402975c60df23c532d92
peterfhenry <peter.henry@relken.com>
2015-10-26 10:26:03 +1100

    Updated PersonDto to use OrganizationDto
    Updated PersonDto to use OrganizationDto for use when a list of Persons is retrieved from the API. If a person is tied to an Org, then an object containing org information is returned.

--- PipedriveNet/Dto/PersonDto.cs
+++ PipedriveNet/Dto/PersonDto.cs
@@ -5,7 +5,7 @@ namespace PipedriveNet.Dto
 	public class PersonDto
 	{
         public int Id { get; set; }
-        public int? OrgId { get; set; }
+        public OrganizationDto OrgId { get; set; }
         public string Name { get; set; }
         public string FistName { get; set; }
         public string LastName { get; set; }
