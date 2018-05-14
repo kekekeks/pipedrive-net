64ff2ef7bb09fd96f0d38668dd7d5f7321c316e2
Mads Dorup <mads@dorup.dk>
2018-05-14 10:00:22 +0200

    Add support for organizations

---
+++ PipedriveNet/Endpoints/OrganizationsEndpoint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace PipedriveNet.Endpoints
+{
+    public class OrganizationsEndpoint<TOrganization>
+    {
+        private readonly ApiClient _client;
+
+        internal OrganizationsEndpoint(ApiClient client)
+        {
+            _client = client;
+        }
+
+        public Task<List<TOrganization>> All
+        {
+            get
+            {
+                return _client.Get<List<TOrganization>>("organizations");
+            }
+        }
+
+        private JObject PrepareUserData(string name, string source, string partner, int ownerId)
+        {
+            return new JObject
+            {
+                [nameof(name)] = name,
+                ["owner_id"] = ownerId,
+                ["b877145c1293686c9832cac035703057a85f2fa8"] = source,
+                ["5d65d158579420f6d46b7d381fad397d74778553"] = partner
+            };
+        }
+
+        public Task<TOrganization> Create(string name, string source, string partner, int ownerId)
+        {
+            var userData = PrepareUserData(name, source, partner, ownerId);
+
+            return _client.Post<TOrganization>("organizations", userData);
+        }
+    }
+}
