c458328dc8bd49ddc5f3941a1dcddd0e10cffa17
Mads Dorup <mads@dorup.dk>
2018-05-15 08:53:52 +0200

    More line breaks

--- PipedriveNet/Dto/OrganizationDto.cs
+++ PipedriveNet/Dto/OrganizationDto.cs
@@ -3,8 +3,11 @@ namespace PipedriveNet.Dto
     public class OrganizationDto
     {
         public int OrgId { get; set; }
+
         public string Name { get; set; }
+
         public int PeopleCount { get; set; }
+
         public string CCEmail { get; set; }
 
         public int Value { set { OrgId = value; } }
